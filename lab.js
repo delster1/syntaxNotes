df8bf13f7d81c987f1a18e1848a07d127c71c7c7
delster1 <delsterone@gmail.com>
2023-03-02 15:46:03 -0600

    created html and tried canvas lol

---
+++ lab.js
@@ -0,0 +1,5 @@
+var canvas = document.createElement("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+document.getElementsByTagName("body")[0].appendChild(canvas);​​​​​​​
+​
